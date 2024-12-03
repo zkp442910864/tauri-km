0b24263a71d6a02cb75facfccb352cebd7689e95
zkp <442910864@qq.com>
2024-12-03 17:29:56 +0800

    init

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,26 @@ import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import { CustomRouter } from './router';
 import { createViewport, getDevice } from './utils/index.ts';
+import { ConfigProvider } from 'antd';
+import zhCN from 'antd/locale/zh_CN';
+import dayjs from 'dayjs';
+import 'dayjs/locale/zh-cn';
+
+import '@unocss/reset/eric-meyer.css';
 import 'virtual:uno.css';
+
 
 void (() => {
     const [, dType,] = getDevice();
     createViewport(dType);
 
+    dayjs.locale('zh-cn');
+
     createRoot(document.getElementById('root')!).render(
         <StrictMode>
-            <RouterProvider router={CustomRouter.getInstance().router!}/>
+            <ConfigProvider locale={zhCN}>
+                <RouterProvider router={CustomRouter.getInstance().router!}/>
+            </ConfigProvider>
         </StrictMode>
     );
 })();
