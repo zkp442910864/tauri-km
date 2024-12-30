c27d5459afb915e28968d6d3ac8f910be4ba7bad
zkp <442910864@qq.com>
2024-12-30 10:07:48 +0800

    fix:修复获取空时,报错

--- src/hooks/modules/useCacheValue.ts
+++ src/hooks/modules/useCacheValue.ts
@@ -15,7 +15,9 @@ export const useCacheValue = <S, >(key: string, valOrFn: S | (() => S)) => {
     useEffect(() => {
         try {
             const val = window.localStorage.getItem(key)!;
-            void setState(JSON.parse(val) as S);
+            if (val) {
+                void setState(JSON.parse(val) as S);
+            }
         }
         catch (error) {
             //
